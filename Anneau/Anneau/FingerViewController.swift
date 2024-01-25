5952212ca51d84130df1001f02c903571e6439e8
filipesdearaujo <123832327+filipesdearaujo@users.noreply.github.com>
2024-01-24 22:39:52 -0300

    calculadora de pontos em arquivo separado

--- Anneau/Anneau/FingerViewController.swift
+++ Anneau/Anneau/FingerViewController.swift
@@ -5,49 +5,17 @@
 //  Created by Filipe Simões on 22/01/24.
 //
 
-// 72 points = 25.4 mm = 1Pol
-// So 1 mm  = 72/25.4 pt = 2,83pt
-// https://forums.developer.apple.com/forums/thread/669276
-
 import UIKit
 
 class FingerViewController: UIViewController {
 
+    var calculatePointToMm = CalculatePointToMm()
+    
     @IBOutlet weak var fingerView: FingerView!
     @IBOutlet weak var valueRing: UILabel!
     @IBOutlet weak var sliderRing: UISlider!
     @IBOutlet weak var valueMm: UILabel!
     
-    let ringSize: [Int: CGFloat] = [
-        1: 12.45,
-        2: 13.26,
-        3: 13.67,
-        4: 14.07,
-        5: 14.27,
-        6: 14.68,
-        7: 14.88,
-        8: 15.29,
-        9: 15.70,
-        10: 16.10,
-        11: 16.51,
-        12: 16.71,
-        13: 16.92,
-        14: 17.32,
-        15: 17.73,
-        16: 18.14,
-        17: 18.54,
-        18: 18.95,
-        19: 19.35,
-        20: 19.76,
-        21: 19.96,
-        22: 20.17,
-        23: 20.57,
-        24: 20.98,
-        25: 21.39,
-        26: 21.79,
-        27: 22.20
-    ]
-
     @IBAction func sliderChanged(_ sender: UISlider) {
         valueRing.text = "\(Int(round(100 * sender.value) / 100))"
         sliderValueChanged()
@@ -60,24 +28,7 @@ class FingerViewController: UIViewController {
     
     @objc func sliderValueChanged() {
         // Atualizar o raio da CircleView quando o valor do slider for alterado
-        fingerView.updateSize(calcularValorParaChave(key:CGFloat(sliderRing.value))!)
-        valueMm.text = "\(updateValorMM(key: CGFloat(sliderRing.value)) ?? 18.14)mm"
+        fingerView.updateSize(calculatePointToMm.calcularValorParaChave(key:CGFloat(sliderRing.value))!)
+        valueMm.text = "\(calculatePointToMm.updateValorMM(key: CGFloat(sliderRing.value)) ?? 18.14)mm"
     }
-    
-    func calcularValorParaChave(key: CGFloat) -> CGFloat? {
-                if let valor = ringSize[Int(key)] {
-                    return CGFloat(valor * 2.83)
-        } else {
-            return nil  // Retorna nil se a chave não existir em ringSize
-        }
-    }
-    
-    func updateValorMM(key: CGFloat) -> CGFloat? {
-                if let valor = ringSize[Int(key)] {
-            return CGFloat(valor)
-        } else {
-            return nil  // Retorna nil se a chave não existir em ringSize
-        }
-    }
-
 }
--- Anneau/Anneau/ViewController.swift
+++ Anneau/Anneau/ViewController.swift
@@ -5,51 +5,17 @@
 //  Created by Filipe Simões on 16/01/24.
 //
 
-// 72 points = 25.4 mm = 1Pol
-// So 1 mm  = 72/25.4 pt = 2,83pt
-// https://forums.developer.apple.com/forums/thread/669276
-
 import UIKit
 
 class ViewController: UIViewController {
 
+    var calculatePointToMm = CalculatePointToMm()
+    
     @IBOutlet weak var valueRing: UILabel!
     @IBOutlet weak var sliderRing: UISlider!
     @IBOutlet weak var circleView: CircleView!
     @IBOutlet weak var valueMm: UILabel!
     @IBOutlet weak var BotaoAnel: UIButton!
-    
-    
-    let ringSize: [Int: CGFloat] = [
-        1: 12.45,
-        2: 13.26,
-        3: 13.67,
-        4: 14.07,
-        5: 14.27,
-        6: 14.68,
-        7: 14.88,
-        8: 15.29,
-        9: 15.70,
-        10: 16.10,
-        11: 16.51,
-        12: 16.71,
-        13: 16.92,
-        14: 17.32,
-        15: 17.73,
-        16: 18.14,
-        17: 18.54,
-        18: 18.95,
-        19: 19.35,
-        20: 19.76,
-        21: 19.96,
-        22: 20.17,
-        23: 20.57,
-        24: 20.98,
-        25: 21.39,
-        26: 21.79,
-        27: 22.20
-    ]
-
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -69,24 +35,8 @@ class ViewController: UIViewController {
 
     @objc func sliderValueChanged() {
         // Atualizar o raio da CircleView quando o valor do slider for alterado
-        circleView.updateRadius(calcularValorParaChave(key:CGFloat(sliderRing.value))!)
-        valueMm.text = "\(updateValorMM(key: CGFloat(sliderRing.value)) ?? 18.14)mm"
-    }
-
-    func calcularValorParaChave(key: CGFloat) -> CGFloat? {
-                if let valor = ringSize[Int(key)] {
-                    return CGFloat(valor * 2.83)
-        } else {
-            return nil  // Retorna nil se a chave não existir em ringSize
-        }
-    }
-
-    func updateValorMM(key: CGFloat) -> CGFloat? {
-                if let valor = ringSize[Int(key)] {
-            return CGFloat(valor)
-        } else {
-            return nil  // Retorna nil se a chave não existir em ringSize
-        }
+        circleView.updateRadius(calculatePointToMm.calcularValorParaChave(key:CGFloat(sliderRing.value))!)
+        valueMm.text = "\(calculatePointToMm.updateValorMM(key: CGFloat(sliderRing.value)) ?? 18.14)mm"
     }
 
 }
